eac0cc79d1d7edf7ed210316619b2e67004dddcf
sol <nathf1005@naver.com>
2024-11-27 22:24:16 +0900

    add: SideBar 생성

---
+++ src/SideBar.jsx
@@ -0,0 +1,14 @@
+export default function SideBar() {
+  return (
+    <div
+      className="fixed top-[-23px] left-0 h-screen w-16 m-0 
+    flex flex-col bg-gray-900 text-white shadow-lg"
+    >
+      <i>A</i>
+      <i>B</i>
+      <i>C</i>
+      <i>D</i>
+      <i>E</i>
+    </div>
+  );
+}
